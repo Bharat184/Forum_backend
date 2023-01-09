3be0a89e0146d0fec933ec5f443fa414ae426fb4
Bharat Chimariya <bharatchimsz@gmail.com>
2023-01-09 18:57:35 +0530

    connection err

--- connection.js
+++ connection.js
@@ -3,7 +3,15 @@ const mongoose=require('mongoose')
 const mongoURI=`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@atlascluster.cotc7xk.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
 const connectToMongo=()=>{
     mongoose.connect(mongoURI,(err)=>{
-        console.log("connected to mongo db successfully");
+        if(err)
+        {
+            console.log("ERROR CONNECTING TO DB");
+        }
+        else
+        {
+            console.log("connected to mongo db successfully");
+
+        }
     })
 }
 module.exports=connectToMongo;
